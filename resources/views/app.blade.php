08c5a5888eae1a427899256161a7d45491ee97fb
GEOSPAU <audacieuxgeospau@gmail.com>
2026-07-22 16:49:00 +0200

    ajout balise google search console

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <meta name="google-site-verification" content="aTvlJrvZq-EdUUif4IFXKTvaHBCcpDv8xy5RClcE6DQ" />
         @viteReactRefresh
         @vite(['resources/js/app.jsx'])
         @inertiaHead
